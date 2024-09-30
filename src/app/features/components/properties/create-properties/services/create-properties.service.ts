984da157765df3a69c752e35d18f40bd9fd1e4cd
jfpa11100 <felipepala1000@gmail.com>
2024-09-29 23:52:33 -0500

    updated gitignore

--- src/app/features/components/properties/create-properties/services/create-properties.service.ts
+++ src/app/features/components/properties/create-properties/services/create-properties.service.ts
@@ -1,6 +1,6 @@
 import { Injectable,NgZone } from '@angular/core';
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
-import { environment } from '../../../../../../environments/environment.development';
+import { environment } from '../../../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
