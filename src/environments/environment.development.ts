173d6fe50a3dfd4a24524791d3cfa2440f3b0152
jfpa11100 <felipepala1000@gmail.com>
2024-09-29 23:50:54 -0500

    delete enviroments keys

--- src/environments/environment.development.ts
+++
@@ -1,7 +0,0 @@
-// Archivo en donde se escriben las keys
-export const environment = {
-    supabase: {
-        url: 'https://lvrhtmkieskuhtgklfia.supabase.co',
-        apikey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx2cmh0bWtpZXNrdWh0Z2tsZmlhIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyNzY0Mzg1OCwiZXhwIjoyMDQzMjE5ODU4fQ.xQkpv2aBdag91xVSpudERSqx8Bg98N_E3zUHhhK1a7w'
-    }
-};
