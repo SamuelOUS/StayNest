1095dcddec3be746f727e943350ee1685b0edfd3
Juan Felipe Palacio <felipepala1000@gmail.com>
2024-11-19 00:29:24 -0500

    create booking logic

--- src/app/features/pages/show-property/show-property.component.ts
+++ src/app/features/pages/show-property/show-property.component.ts
@@ -8,6 +8,10 @@ import { Review } from '../../interfaces/review.interface';
 import { UserService } from '../../../auth/services/user.service';
 import Swal from 'sweetalert2';
 import { StarComponent } from "../../components/star/star.component";
+import { BookingsService } from '../../services/bookings.service';
+import { LogInComponent } from '../../../auth/components/log-in/log-in.component';
+import { ModalService } from '../../../services/modal.service';
+import { catchError, tap, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-show-property',
@@ -17,24 +21,9 @@ import { StarComponent } from "../../components/star/star.component";
   styleUrls: ['./show-property.component.css']
 })
 export class ShowPropertyComponent implements OnInit {
-  property: Property = {
-    title: '',
-    address: '',
-    description: '',
-    bedrooms: 0,
-    bathrooms: 0,
-    capacity: 0,
-    pricePerNight: 0,
-    photos: [],
-    reviews: [],
-    user: {
-      profilePicture: '',
-      name: '',
-      email: '',
-    }
-  };
+  property!: Property
   newReviewComment: string = '';
-  newRating: number = 5; // Inicia con un valor válido
+  newRating: number = 5;
   propertyId: string = '';
   isModalOpen: boolean = false; // Controla la visibilidad del modal
   selectedStartDate!: string; // Almacena la fecha de inicio
@@ -43,37 +32,23 @@ export class ShowPropertyComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private propertyService: PropertyService,
-    private userService: UserService
+    private userService: UserService,
+    private bookingsService: BookingsService,
+    private modalService: ModalService
   ) {}
 
   ngOnInit(): void {
     this.propertyId = this.route.snapshot.paramMap.get('id')!;
     this.propertyService.getProperty(this.propertyId).subscribe((property) => {
-      this.property = property || this.getDefaultProperty();
+      this.property = property
     });
   }
 
-  private getDefaultProperty(): Property {
-    return {
-      title: '',
-      address: '',
-      description: '',
-      bedrooms: 0,
-      bathrooms: 0,
-      capacity: 0,
-      pricePerNight: 0,
-      photos: [],
-      reviews: [],
-      user: {
-        profilePicture: '',
-        name: '',
-        email: '',
-      }
-    };
-  }
-
   openModal(): void {
-    this.isModalOpen = true;
+    if (this.userService.getUser()().username)
+      this.isModalOpen = true;
+    else
+      this.modalService.openModal<LogInComponent, null>(LogInComponent);
   }
 
   closeModal(): void {
@@ -81,36 +56,57 @@ export class ShowPropertyComponent implements OnInit {
   }
 
   submitReservation(): void {
-    if (this.selectedStartDate && this.selectedEndDate) {
-      const start = new Date(this.selectedStartDate);
-      const end = new Date(this.selectedEndDate);
-
-      // Verificar si la fecha de inicio es anterior a la fecha de fin
-      if (start <= end) {
-        Swal.fire({
-          icon: 'success',
-          text: `Reserva realizada del ${this.selectedStartDate} al ${this.selectedEndDate}`
-        });
-        this.closeModal();
-      } else {
-        Swal.fire({
-          icon: 'error',
-          text: 'La fecha de inicio no puede ser posterior a la fecha de fin.'
-        });
-      }
-    } else {
+    if (!this.selectedStartDate && !this.selectedEndDate) {
       Swal.fire({
         icon: 'error',
         text: 'Por favor selecciona las fechas de inicio y fin.'
       });
+      return;
     }
+    
+    const start = new Date(this.selectedStartDate);
+    const end = new Date(this.selectedEndDate);
+    if (start > end) {
+      Swal.fire({
+        icon: 'error',
+        text: 'La fecha de inicio no puede ser posterior a la fecha de fin.'
+      });
+      this.closeModal();
+      return
+    }
+
+    const differenceInMilliseconds = end.getTime() - start.getTime();
+    const differenceInDays = differenceInMilliseconds / (1000 * 60 * 60 * 24);
+    const totalPrice = differenceInDays * this.property.pricePerNight;
+
+    this.bookingsService.createBooking({
+      propertyId: this.propertyId,
+      startDate: start,
+      endDate: end, 
+      totalPrice
+    }).pipe(
+      catchError(err => throwError(() => {
+        Swal.fire({
+          icon: 'error',
+          text: 'No se pudo realizar la reserva'
+        })
+        return
+      }))).subscribe(
+        data =>{
+          Swal.fire({
+            icon: 'success',
+            text: `Reserva realizada del ${this.selectedStartDate} al ${this.selectedEndDate}. Total a pagar: ${totalPrice}`
+          });
+        }
+      )
+    this.closeModal();
   }
 
   addReview(): void {
     if (this.newReviewComment.trim()) {
       const newReview: Review = {
         propertyId: this.propertyId,
-        rating: this.newRating || 5, // Usa valor por defecto si newRating es undefined
+        rating: this.newRating,
         comment: this.newReviewComment,
         createdAt: new Date(Date.now())
       };
